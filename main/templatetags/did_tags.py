eafd284b61fe4b33872a64294909473c24be5937
bobby-didcoding <80459294+bobby-didcoding@users.noreply.github.com>
2021-05-07 10:18:30 +0100

    debugged

--- main/templatetags/did_tags.py
+++ main/templatetags/did_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def unix(value):
 
 	# you can pass the following obj to a DateTimeField, when your settings.USE_TZ == True
-	datetime_obj_with_tz = make_aware(datetime.fromtimestamp(value)).date()
+	datetime_obj_with_tz = make_aware(datetime.fromtimestamp(value))
 	return datetime_obj_with_tz
 
 register.filter('unix', unix)
